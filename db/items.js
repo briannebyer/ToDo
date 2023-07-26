c1cd0d313f99a0cec069b29020e024f52de85f23
briannebyer <briannemareebyer@mail.com>
2023-07-26 12:41:44 +1000

    ensure correct db connection and fix typos

--- db/items.js
+++ db/items.js
@@ -2,19 +2,19 @@
 const knex = require("./knex");
 
 function createItem(item){
-    return knex("TODOITEMS").insert(item);
+    return knex("TODO_ITEMS").insert(item);
 };
 
 function getAllItems(){
-    return knex("TODOITEMS").select("*");
+    return knex("TODO_ITEMS").select("*");
 };
 
 function deleteItem(ID){
-    return knex("TODOITEMS").where("ID", ID).del();
+    return knex("TODO_ITEMS").where("ID", ID).del();
 };
 
 function updateItem(ID, item) {
-    return knex("TODOITEMS").where("ID", ID).update(item);
+    return knex("TODO_ITEMS").where("ID", ID).update(item);
 };
 
 // executable against ToDo table
